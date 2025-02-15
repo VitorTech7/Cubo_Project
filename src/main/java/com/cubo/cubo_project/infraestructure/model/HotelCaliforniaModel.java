f38da376038f43389d65d6a05b38f691af4735da
vitor.martins <vitor.martins@mv.com.br>
2025-02-15 17:57:24 -0300

    Criando a entidade e o nome da tabela e as respectivas colunas.

--- src/main/java/com/cubo/cubo_project/infraestructure/model/HotelCaliforniaModel.java
+++ src/main/java/com/cubo/cubo_project/infraestructure/model/HotelCaliforniaModel.java
@@ -1,16 +1,35 @@
 package com.cubo.cubo_project.infraestructure.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
+
+@Table(name = "hotel_california")
 public class HotelCaliforniaModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @Column(name = "nome")
     private String name;
 
+    @Column(name = "local")
     private String local;
 
+    @Column(name = "capacidade")
     private int capacidade;
 
+    @Column(name = "cnpj")
     private String cnpj;
 }
