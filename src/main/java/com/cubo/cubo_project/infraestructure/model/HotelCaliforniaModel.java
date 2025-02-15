b6ac1d24b0076fbc602d8bd9246e8bd43b3cff4b
vitor.martins <vitor.martins@mv.com.br>
2025-02-15 15:15:11 -0300

    Criando o model da aplicação

---
+++ src/main/java/com/cubo/cubo_project/infraestructure/model/HotelCaliforniaModel.java
@@ -0,0 +1,16 @@
+package com.cubo.cubo_project.infraestructure.model;
+
+import java.util.UUID;
+
+public class HotelCaliforniaModel {
+
+    private UUID id;
+
+    private String name;
+
+    private String local;
+
+    private int capacidade;
+
+    private String cnpj;
+}
