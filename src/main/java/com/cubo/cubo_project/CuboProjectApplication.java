06ed4d1ef892b26a1b3ef8e64a535c9fc9a1add1
vitor.martins <vitor.martins@mv.com.br>
2025-02-15 15:04:12 -0300

    Criando projeto do cubo

---
+++ src/main/java/com/cubo/cubo_project/CuboProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cubo.cubo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuboProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuboProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cubo/cubo_project/CuboProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubo.cubo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuboProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
