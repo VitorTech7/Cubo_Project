1b500c08ecb250e644bfa7442d00dc1fbc1300a7
vitor.martins <vitor.martins@mv.com.br>
2025-02-26 13:35:31 -0300

    Retirado o Lombok da aplicação (Só causa problema)

--- src/main/java/com/cubo/cubo_project/infraestructure/model/HotelCaliforniaModel.java
+++ src/main/java/com/cubo/cubo_project/infraestructure/model/HotelCaliforniaModel.java
@@ -1,26 +1,13 @@
 package com.cubo.cubo_project.infraestructure.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.io.Serializable;
 
 
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "hotel_california")
 public class HotelCaliforniaModel implements Serializable {
-
-    public HotelCaliforniaModel(){
-
-    }
-    public HotelCaliforniaModel(String nome, String local, int capacidade, String cnpj) {
-        this.nome = nome;
-        this.local = local;
-        this.capacidade = capacidade;
-        this.cnpj = cnpj;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,13 +25,20 @@ public class HotelCaliforniaModel implements Serializable {
     @Column(name = "cnpj")
     private String cnpj;
 
+    public HotelCaliforniaModel(){
+
+    }
+    public HotelCaliforniaModel(String nome, String local, int capacidade, String cnpj) {
+        this.nome = nome;
+        this.local = local;
+        this.capacidade = capacidade;
+        this.cnpj = cnpj;
+    }
+
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
--- src/main/java/com/cubo/cubo_project/infraestructure/service/HotelCaliforniaService.java
+++ src/main/java/com/cubo/cubo_project/infraestructure/service/HotelCaliforniaService.java
@@ -3,12 +3,10 @@ package com.cubo.cubo_project.infraestructure.service;
 import com.cubo.cubo_project.infraestructure.model.HotelCaliforniaModel;
 import com.cubo.cubo_project.infraestructure.repository.HotelCaliforniaRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class HotelCaliforniaService {
